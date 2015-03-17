412dee37bd196b5fed498eb1eb65695ebddc8057
alumno04 <alumno04@TPP-LAB04-11.TPP.UPEU.EDU.PE>
2015-03-17 10:26:17 -0500

    creado el proyecto PruebaGitHup y se esta agregando por primera vez al Git.

---
+++ PruebaGitHup/src/pruebagithup/PruebaGitHup.java
@@ -0,0 +1,17 @@
+
+package pruebagithup;
+
+/**
+ *
+ * @author alumno04
+ */
+public class PruebaGitHup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
